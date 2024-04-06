c3833ffa9b48aed6778eb4e6b44d2f33cad2f08e
leesin1040 <leesin1040@gmail.com>
2024-04-07 02:06:11 +0900

    first commit

---
+++ fruits.js
@@ -0,0 +1,48 @@
+const FRUITS = [
+  {
+    name: 'base/00_cherry',
+    radius: 46 / 2,
+  },
+  {
+    name: 'base/01_strawberry',
+    radius: 55 / 2,
+  },
+  {
+    name: 'base/02_grape',
+    radius: 67 / 2,
+  },
+  {
+    name: 'base/03_gyool',
+    radius: 81 / 2,
+  },
+  {
+    name: 'base/04_orange',
+    radius: 97 / 2,
+  },
+  {
+    name: 'base/05_apple',
+    radius: 117 / 2,
+  },
+  {
+    name: 'base/06_pear',
+    radius: 142 / 2,
+  },
+  {
+    name: 'base/07_peach',
+    radius: 171 / 2,
+  },
+  {
+    name: 'base/08_pineapple',
+    radius: 206 / 2,
+  },
+  {
+    name: 'base/09_melon',
+    radius: 249 / 2,
+  },
+  {
+    name: 'base/10_watermelon',
+    radius: 300 / 2,
+  },
+];
+
+export { FRUITS };
